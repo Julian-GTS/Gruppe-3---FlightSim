741d7cb4d36018554ec80a7f27796b9d67681ca6
philipp1669 <113363863+philipp1669@users.noreply.github.com>
2024-02-21 21:08:39 +0100

    Neues Model FlightData erstellt, das testweise Flugdaten speichert und darstellen soll

--- SimConnect/upload_jason/models.py
+++ SimConnect/upload_jason/models.py
@@ -3,4 +3,10 @@ from django.db import models
 class JsonData(models.Model):
     data = models.JSONField()
 
-   
+class FlightData(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+    packetTime = models.TimeField()
+
+
