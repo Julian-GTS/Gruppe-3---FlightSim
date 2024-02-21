6a25afaf69d41728f711428c8e9021057134ce91
philipp1669 <113363863+philipp1669@users.noreply.github.com>
2024-02-21 21:33:37 +0100

    Code cleanup, Verzicht auf HTML-Seiten, direkte JSON-Darstellung, Anzeige der Timestamps

--- SimConnect/upload_jason/views.py
+++ SimConnect/upload_jason/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import JsonData, FlightData
-import datetime
-import json
+import json, datetime
 
 def index(request):
     if request.GET.get("page", "") == "table":
-        return render(request, "table.html", {
-            "data": JsonData.objects.values_list
-        })
+        list = JsonData.objects.values_list()
+        print(list)
+        result = {
+            "data": [json.loads(entry[1]) for entry in list],
+            "timestamps": [entry[2] for entry in list] 
+        }
+        return JsonResponse(result)
     if request.GET.get("page", "") == "testpage":
-        FlightData.objects.create(
-            x = 1.2,
-            y = 5.4,
-            z = 6.9,
-            packetTime = datetime.datetime.now()
-        )
+        # FlightData.objects.create(
+        #     x = 1.2,
+        #     y = 5.4,
+        #     z = 6.9,
+        #     packetTime = datetime.datetime.now()
+        # )
 
-        result = FlightData.objects.values()             # return ValuesQuerySet object
-        list_result = [entry for entry in result]  # converts ValuesQuerySet into Python list
-        print(list_result[0])
-        return render(request, "test.html", {
-            "data": list_result
-        })
+        list_result = [entry for entry in FlightData.objects.values()]  # converts ValuesQuerySet into Python list
+
+        return JsonResponse({"data": list_result})
     if request.method == 'POST':
         json_file = request.FILES['json_file']
         data = json_file.read().decode('utf-8')
-        JsonData.objects.create(data=data).save()
+        JsonData.objects.create(data=data, timestamp=datetime.datetime.now()).save()
         return render(request, 'success.html', {
             "testVersion": data
         })
