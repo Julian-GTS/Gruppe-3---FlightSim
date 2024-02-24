85ccad660174f3ec6b4c708e7bea54d2a20558d3
Philipp Gutsche <113363863+philipp1669@users.noreply.github.com>
2024-02-24 17:09:59 +0100

    add table path to upload jason

--- SimConnect/upload_jason/views.py
+++ SimConnect/upload_jason/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import JsonData, FlightData
-import json, datetime
+import json
+from django.utils import timezone
 
 def index(request):
-    if request.GET.get("page", "") == "table":
-        list = JsonData.objects.values_list()
-        result = {
-            "data": [json.loads(entry[1]) for entry in list],
-            "timestamps": [entry[2] for entry in list] 
-        }
-        return JsonResponse(result)
     if request.GET.get("page", "") == "testpage":
 
         list_result = [entry for entry in FlightData.objects.values()] # converts ValuesQuerySet into Python list
@@ -19,16 +13,16 @@ def index(request):
     if request.method == 'POST':
         json_file = request.FILES['json_file']
         data = json_file.read().decode('utf-8')
-        JsonData.objects.create(data=data, timestamp=datetime.datetime.now()).save()
+        JsonData.objects.create(data=data, timestamp=timezone.now()).save()
         return render(request, 'success.html', {
             "testVersion": data
         })
     return render(request, "upload.html")
 
-
-        # FlightData.objects.create(
-        #     x = 1.2,
-        #     y = 5.4,
-        #     z = 6.9,
-        #     packetTime = datetime.datetime.now()
-        # )
+def table(request):
+    list = JsonData.objects.values_list()
+    result = {
+        "data": [json.loads(entry[1]) for entry in list],
+        "timestamps": [entry[2] for entry in list] 
+    }
+    return JsonResponse(result)
