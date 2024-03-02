41ba7bf355ccf34342d0ca8bda82bd04139bc433
Philipp Gutsche <113363863+philipp1669@users.noreply.github.com>
2024-03-02 13:31:56 +0100

    Add redirect_latest endpoint to api urls.py

--- SimConnect/api/urls.py
+++ SimConnect/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.redirect_latest, name="api-redir-latest"),
     path("v1/", views.root, name="api-root"),
     path("v1/tree", views.tree, name="endpoint-list"),
 ]
--- SimConnect/api/views.py
+++ SimConnect/api/views.py
@@ -3,6 +3,9 @@ from django.http import FileResponse, Http404, HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from . import api_response, urls
 
+def redirect_latest(request):
+    return redirect("api-root")
+
 def root(request):
     response = api_response.APIResponse(True, ["Welcome to the API!", "Endpoint list: /api/v1/tree"])
     return JsonResponse(response.to_json())
