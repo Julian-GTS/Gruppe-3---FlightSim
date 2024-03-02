03fa19a6bc28a3ce018e4570abcfb39e8f3ed7e4
Philipp Gutsche <113363863+philipp1669@users.noreply.github.com>
2024-03-02 10:54:46 +0100

    fix base path

--- SimConnect/api/urls.py
+++ SimConnect/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("v1", views.root, name="api-root"),
+    path("v1/", views.root, name="api-root"),
     path("v1/tree", views.tree, name="endpoint-list"),
 ]
